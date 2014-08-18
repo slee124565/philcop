227664bb2caf681f03f22a3d34c84b96eee174b1
Lee Shiueh <slee@smartic.com.cn>
2014-08-18 11:09:01 +0800

    add single fund fail update retry task for chain_update task

--- fundclear/tasks.py
+++ fundclear/tasks.py
@@ -17,14 +17,22 @@ def chain_update_taskhandler(request):
     logging.info(__name__ + ', chain_update_taskhandler ' + request.method + ' with index ' + str(t_index))
     
     #-> update fund with t_index
-    t_fund_info_list = get_fundcode_list()[:100]
+    t_fund_info_list = get_fundcode_list()[:150]
     #t_fund_info_list = [[0,'a','a1'],[1,'b','b1'],[2,'c','c1']]
     if t_index < len(t_fund_info_list):
         
         #-> udpate FundClearModel
         t_fundinfo = t_fund_info_list[t_index]
         logging.debug(__name__ + ', chain_update_taskhandler: fund code ' + t_fundinfo[CODE_INDEX])
-        FundClearModel.get_fund(t_fundinfo[CODE_INDEX])
+        t_fund = FundClearModel.get_fund(t_fundinfo[CODE_INDEX])
+        if t_fund is None:
+            logging.info(__name__ + ', chain_update_taskhandler add update_task for fund_id ' + t_fundinfo[CODE_INDEX])
+            t_taskhandler_path = os.path.dirname(os.path.dirname(request.get_full_path())) + '/update/' + t_fundinfo[CODE_INDEX] + '/'
+            taskqueue.add(method = 'GET', \
+                          url = t_taskhandler_path, \
+                          countdown = 5, \
+                          )
+            
 
         #-> add next chain_task
         t_index += 1
