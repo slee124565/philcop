95a0ebc88ee89a25d758b492bd9d7ce90fb1947f
Lee Shiueh <slee@smartic.com.cn>
2014-09-24 13:32:48 +0800

    translated message ready for compile

--- phicops/settings.py
+++ phicops/settings.py
@@ -76,7 +76,8 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 #LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh-tw'
+#LANGUAGE_CODE = 'zh-tw'
+LANGUAGE_CODE = 'zh-cn'
 
 TIME_ZONE = 'UTC'
 
