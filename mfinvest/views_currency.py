2d7381a5c128fd967f4c536509c42817e6699464
Lee Shiueh <slee@smartic.com.cn>
2014-10-23 18:23:27 +0800

    add TWD Exchange View

--- mfinvest/views_currency.py
+++ mfinvest/views_currency.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.utils.translation import ugettext as _
 
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -7,7 +8,7 @@ from dateutil.relativedelta import relativedelta
 from bankoftaiwan.models import BotExchangeModel
 from utils import util_date
 import bankoftaiwan.exchange as bot
-import calendar, logging
+import calendar, logging, collections
  
 def tw_exchange_changing_view(request):
     target_currency = [bot.CURRENCY_CNY, bot.CURRENCY_USD, bot.CURRENCY_JPY]
@@ -16,7 +17,13 @@ def tw_exchange_changing_view(request):
     #date_list = util_date.get_sample_date_list_2(p_date_begin=(date.today()+relativedelta(years=-1)), \
     #                                             p_date_end=date.today(), \
     #                                             p_inc_everyday_of_last_month=True)
+    content_head_list = ['Date'] + target_currency
+    content_rows = {}
+
     date_list = util_date.get_sample_date_list()
+    for t_date in date_list:
+        content_rows[t_date.strftime('%Y%m%d')] = (t_date.strftime('%Y/%m/%d'),)
+    
     tw_exchanges = {}
     for t_currency in target_currency:
         #logging.debug('currency: ' + str(t_currency))
@@ -26,6 +33,7 @@ def tw_exchange_changing_view(request):
         #logging.debug(str(tw_exchanges[t_currency]))
         prev_rate = tw_exchanges[t_currency][0][1]
         for ndx, t_entry in enumerate(tw_exchanges[t_currency]):
+            content_rows[t_entry[0].strftime('%Y%m%d')] += ('{:.4f}'.format(t_entry[1]),)
             this_rate = t_entry[1]
             if t_entry[1] == 0:
                 t_entry[1] = 0
@@ -45,11 +53,18 @@ def tw_exchange_changing_view(request):
     plot = {
             'data': plot_data 
             }
+    
+    content_rows = collections.OrderedDict(sorted(content_rows.items()))
+    tbl_content = {
+                   'heads': content_head_list,
+                   'rows': content_rows.values(),
+                   }
 
     args = {
-            'tpl_img_header' : str(target_currency) + ' Exchange Rate Changing with TWD' ,
+            'tpl_img_header' : _('HEADER_EX_RATE_CHANGE_WITH_TWD'), #str(target_currency) + ' Exchange Rate Changing with TWD' ,
             'plot' : plot,
-            'tpl_section_title' : 'Developed by lee.shiueh@gmail.com',
+            'tpl_section_title' : _('TITLE_BOT_CURRENCY_CASH_SELL'), 
+            'tbl_content' : tbl_content,
             }
     
     return render_to_response('mf_simple_flot.tpl.html',args)
