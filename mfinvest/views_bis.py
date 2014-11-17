ac3e823cb2af3d90960494e75a716a355a661d8e
Lee Shiueh <slee@smartic.com.cn>
2014-11-17 11:28:29 +0800

    add EERs index table

--- mfinvest/views_bis.py
+++ mfinvest/views_bis.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
+#from django.http import HttpResponse
 from bis_org.models import BisEersModel
 from utils import util_bollingerbands
+from django.utils.translation import ugettext as _
 import calendar
 
 def eers_view(request, p_code='TW'):
@@ -9,7 +11,19 @@ def eers_view(request, p_code='TW'):
     area_eers = bis_eers.get_area_real_bis_eers(p_area)
 
     t_value_list = area_eers[p_area]
-    #return HttpResponse(str(t_value_list))
+    #return HttpResponse(str(t_value_list[-24:]))
+    
+    #-> indices table content
+    content_head_list = ['Date', 'Indices']
+    content_rows = []
+    for t_entry in t_value_list[-24:]:
+        content_rows.append([t_entry[0].strftime('%Y/%m/%d'), t_entry[1]])
+    tbl_content = {
+                   'heads': content_head_list,
+                   'rows': content_rows,
+                   }
+
+    #-> indices BB content
     sma,tb1,tb2,bb1,bb2 = util_bollingerbands.get_bollingerbands(t_value_list)
 
     for ndx2, t_list in enumerate([t_value_list,sma,tb1,tb2,bb1,bb2]):
@@ -34,7 +48,8 @@ def eers_view(request, p_code='TW'):
     args = {
             'tpl_img_header' :  p_area + ' BIS EERs Indices Bollinger Band Review',
             'plot' : plot,
-            'tpl_section_title' : 'Developed by lee.shiueh@gmail.com',
+            'tpl_section_title' : _('BIS EERs Indices'), 
+            'tbl_content' : tbl_content,
             }
     
     return render_to_response('mf_simple_flot.tpl.html',args)
