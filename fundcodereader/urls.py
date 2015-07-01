1495289a961d5ff6dfe661c0d7d8d7950f7c2843
Lee Shiueh <lee.shiueh@gmail.com>
2015-07-01 12:52:22 +0800

    add codename view with csv format URL append '/codename/'

--- fundcodereader/urls.py
+++ fundcodereader/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
      url(r'^task/update/$', 'fundcodereader.tasks.update_codelist'),
      url(r'^task/download/$', 'fundcodereader.tasks.update_fundcode_taskhandler'),
 
+     url(r'^codename/$', 'fundcodereader.views.codename_list'),
+
      url(r'^func/test1/$', 'fundcodereader.tests.test_get_codename_list'),
      url(r'^func/test2/$', 'fundcodereader.tests.test_get_fundname'),
      url(r'^func/test3/$', 'fundcodereader.tests.print_content'),
--- fundcodereader/views.py
+++ fundcodereader/views.py
@@ -1,5 +1,14 @@
-'''
-Created on 2014¦~11¤ë6¤é
+from django.http import HttpResponse
 
-@author: Shiueh
-'''
+from models import FundCodeModel
+
+def codename_list(self):
+    response = HttpResponse(content_type='text/plain')
+    t_content = ''
+    t_list = FundCodeModel.get_codename_list()
+    for t_entry in t_list:
+        t_code,t_name = t_entry
+        t_content += t_code + ',' + t_name + '\n'
+        
+    response.content = t_content
+    return response
